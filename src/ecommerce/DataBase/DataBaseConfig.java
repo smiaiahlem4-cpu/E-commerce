f2a113fe6736cec51a7721457e636c64f7522519
smiaiahlem4-cpu <smiaiahlem4@gmail.com>
2026-03-27 01:11:47 +0100

    databaseconfig file

--- src/ecommerce/DataBase/DataBaseConfig.java
+++ src/ecommerce/DataBase/DataBaseConfig.java
@@ -4,7 +4,7 @@ public class DataBaseConfig {
     public static final String Nom_Driver="com.mysql.cj.jdbc.Driver";
     public static final String IPServeur="localhost";
     public static final String PORT="3306";
-    public static final String DataBaseName="tpjavaDB";
+    public static final String DataBaseName="ecommerce";
     public static final String url_db="jdbc:mysql://"+IPServeur+":"+PORT+"/"+DataBaseName;
     public static final String username="root";
     public static final String PASSWORD="";
